02a4ff675cb0a1c46972e65b8a05a81670bc6765
MASRAFE BIN HANNAN SIAM <masrafesiammbhs1633@gmail.com>
2022-05-21 19:40:48 +0600

    Practice

---
+++ Practice/Practice 2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{	
+	int a;	
+	printf("Enter the number: ");
+	scanf("%d",&a);
+	printf("Number:%d",a);
+	return 0;	
+}
---
+++ Practice/Practice 3.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the Number a:");
+	scanf("%d",&a);
+	printf("Enter the Number b:");
+	scanf("%d",&b);
+	printf("Number a=%d\n",a);
+	printf("Number b=%d",b);
+	return 0;	
+}
