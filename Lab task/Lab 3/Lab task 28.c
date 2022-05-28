e8a82fcab2c6de824e5bfb30a0ec8c7220b3de33
MASRAFE BIN HANNAN SIAM <masrafesiammbhs1633@gmail.com>
2022-05-28 14:08:11 +0600

    Lab 3

---
+++ Lab task/Lab 3/Lab task 28.c
@@ -0,0 +1,21 @@
+#include<stdio.h>  
+int main()
+{    
+int a,i,b=0,f=0,g;    
+printf("Enter the number: ");    
+scanf("%d",&a);  
+b=a/2;   
+for(i=2;i<=b;i++)   
+{  
+g=a%i;
+if(g==0)    
+{    
+printf("%d is not a prime number",a);    
+f=1;    
+break;    
+}    
+}    
+if(f==0)    
+printf("%d is a prime number",a);     
+return 0;  
+}
