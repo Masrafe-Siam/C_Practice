a94a7bac910c5a57c63dba98ad80d33976de0fb0
MASRAFE BIN HANNAN SIAM <masrafesiammbhs1633@gmail.com>
2022-06-02 16:50:13 +0600

    Switch case

---
+++ Lab task/Switch case/Switch case 1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter number:");
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 1:
+		printf("Iron");	
+		break;
+		case 2:
+		printf("Bronze");	
+		break;
+		case 3:
+		printf("Silver");	
+		break;
+    	case 4:
+		printf("Gold");
+		break;	
+		case 5:
+		printf("Platinum");
+		break;
+		case 6:
+		printf("Diamond");
+		break;
+		case 7:
+		printf("Immortal");
+		break;
+		case 8:
+		printf("Radiant");
+		break;	
+		deafult:
+		printf("GG");				
+	}
+	return 0;
+}
