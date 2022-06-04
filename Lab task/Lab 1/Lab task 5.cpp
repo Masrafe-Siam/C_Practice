3be694583817f94f973bdf5f627d3ca00bbf4dcb
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-04 13:47:02 +0600

    Update Lab task 5.cpp

--- Lab task/Lab 1/Lab task 5.cpp
+++ Lab task/Lab 1/Lab task 5.cpp
@@ -1,3 +1,7 @@
+/*5. The length & breadth of a rectangle and radius of a circle are input through the keyboard. 
+Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.*/
+
+
 #include<stdio.h>
 #define pi 3.1416
 main()
