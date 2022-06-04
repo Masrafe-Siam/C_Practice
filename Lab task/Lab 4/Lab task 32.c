5eef2f64a03178dda2350272733becfe1696742f
MASRAFE BIN HANNAN SIAM <masrafesiammbhs1633@gmail.com>
2022-06-04 12:52:07 +0600

    Lab 4

---
+++ Lab task/Lab 4/Lab task 32.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	int a,b,add,mul,sub,div;
+    printf("Enter operation:");
+	scanf("%c",&ch);	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	switch(ch)
+	{
+		case '+':
+		add=a+b;	
+		printf("Calculation=%d",add);	
+		break;
+		case '-':
+		sub=a-b;	
+		printf("Calculation=%d",sub);
+		break;
+		case '*':
+		mul=a*b;
+	    printf("Calculation=%d",mul);	
+		break;
+		case '/':
+		div=a/b;	
+		printf("Calculation=%d",div);
+		break;	
+		deafult:
+		printf("No case");			
+	}
+	return 0;	
+}
---
+++ Lab task/Lab 4/Lab task 33.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int ch,i,a,fac=1,b,g,f;
+printf("1:Factorial of a number\n");
+printf("2:Prime or not\n");
+printf("3:Odd or Even\n");
+printf("4:Exit\n");
+printf("Choose the number: ");
+scanf("%d",&ch);
+printf("\n\n");
+switch (ch)
+{	
+case 1:	
+printf("Enter a number: ");    
+scanf("%d",&a);    
+for(i=1;i<=a;i++)
+{    
+fac=fac*i;    
+}
+printf("Factorial of %d is: %d",a,fac);
+break;
+case 2:
+printf("Enter the number: ");    
+scanf("%d",&a);  
+b=a/2;   
+for(i=2;i<=b;i++)   
+{  
+g=a%i;
+if(g==0)    
+{    
+printf("%d is not a prime number",a);    
+f=1;    
+break;    
+}    
+}    
+if(f==0)    
+printf("%d is a prime number",a);  
+break;
+case 3:
+printf("Enter the number :");
+scanf("%d",&a);
+if(a%2==0)
+{	
+printf("Even Number");	
+}
+else
+{
+printf("Odd Number");
+}
+break;
+deafult:
+printf("No case");
+return 0;
+}
+}
