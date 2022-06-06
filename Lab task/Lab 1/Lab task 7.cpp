ebf9436419318ee83cce5dbf734236a1308d603a
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-06 11:17:35 +0600

    If a five-digit number is input through the keyboard, write a program to calculate the sum of its digits.

--- Lab task/Lab 1/Lab task 7.cpp
+++ Lab task/Lab 1/Lab task 7.cpp
@@ -1,4 +1,4 @@
-//7. If a five-digit number is input through the keyboard, write a program to calculate the sum of its digits.
+//7.If a five-digit number is input through the keyboard, write a program to calculate the sum of its digits.
 
 
 #include<stdio.h>
