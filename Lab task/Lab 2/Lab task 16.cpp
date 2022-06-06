4f15c9f5558bc87649e6747bd35e40bc885291fc
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-06 11:18:49 +0600

    Any year is input through the keyboard. Write a program to determine whether the year is a leap year or not.

--- Lab task/Lab 2/Lab task 16.cpp
+++ Lab task/Lab 2/Lab task 16.cpp
@@ -1,4 +1,4 @@
-//3. Any year is input through the keyboard. Write a program to determine whether the year is a leap year or not. 
+//3.Any year is input through the keyboard. Write a program to determine whether the year is a leap year or not. 
 
 
 #include<stdio.h>
