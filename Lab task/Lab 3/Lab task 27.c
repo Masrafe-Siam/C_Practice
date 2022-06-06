7e98f1370de32445ff6c933198606273adc6182c
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-06 11:22:00 +0600

    Write a program to add first seven terms of the following series using a for loop: 1/1! + 2/2! + 3/3! + .............

--- Lab task/Lab 3/Lab task 27.c
+++ Lab task/Lab 3/Lab task 27.c
@@ -1,4 +1,4 @@
-/*4. Write a program to add first seven terms of the following series using a for loop:
+/*4.Write a program to add first seven terms of the following series using a for loop:
 1/1! + 2/2! + 3/3! + .............*/
 
 
