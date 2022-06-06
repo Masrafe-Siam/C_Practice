75fdd89403bd3809ac20c8e69278f63de1316239
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-06 11:14:46 +0600

    Niloys basic salary is input through the keyboard. His dearness allowance is 40% of his basic salary and house rent is 20% of his basic salary. Write a program to calculate his gross salary.

--- Lab task/Lab 1/Lab task 1.cpp
+++ Lab task/Lab 1/Lab task 1.cpp
@@ -1,5 +1,5 @@
-//1.Niloys basic salary is input through the keyboard. His dearness allowance is 40% of his basic salary and house rent is 20% of his basic salary.
-//Write a program to calculate his gross salary.
+/*1.Niloys basic salary is input through the keyboard. His dearness allowance is 40% of his basic salary and house rent is 20% of his basic salary.
+Write a program to calculate his gross salary.*/
 
 
 #include<stdio.h>
