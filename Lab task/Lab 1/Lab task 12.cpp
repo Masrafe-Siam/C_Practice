2af99c99f5b43cd95a631af0bfedd907108847d5
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-06 11:15:40 +0600

    If the total selling price of 15 items and the total profit earned on them is input through the keyboard, write a program to find the cost price of one item.

--- Lab task/Lab 1/Lab task 12.cpp
+++ Lab task/Lab 1/Lab task 12.cpp
@@ -1,4 +1,4 @@
-//12. If the total selling price of 15 items and the total profit earned on them is input through the keyboard, write a program to find the cost price of one item.
+//12.If the total selling price of 15 items and the total profit earned on them is input through the keyboard, write a program to find the cost price of one item.
 
 
 #include<stdio.h>
