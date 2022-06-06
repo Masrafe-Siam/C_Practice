d28ef6a251a5eb7c75e9df0c5107781dbd7cb718
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-06 11:18:06 +0600

    If a four-digit number is input through the keyboard, write a program to obtain the sum of the first and last digit of this number.

--- Lab task/Lab 1/Lab task 9.cpp
+++ Lab task/Lab 1/Lab task 9.cpp
@@ -1,4 +1,4 @@
-//9. If a four-digit number is input through the keyboard, write a program to obtain the sum of the first and last digit of this number.
+//9.If a four-digit number is input through the keyboard, write a program to obtain the sum of the first and last digit of this number.
 
 
 
