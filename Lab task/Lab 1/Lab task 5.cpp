8cc80197bf9e491cd4f27268bdcdfa226649fdd3
Masrafe-Siam <105535162+Masrafe-Siam@users.noreply.github.com>
2022-06-06 11:17:05 +0600

    The length & breadth of a rectangle and radius of a circle are input through the keyboard.  Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.

--- Lab task/Lab 1/Lab task 5.cpp
+++ Lab task/Lab 1/Lab task 5.cpp
@@ -1,4 +1,4 @@
-/*5. The length & breadth of a rectangle and radius of a circle are input through the keyboard. 
+/*5.The length & breadth of a rectangle and radius of a circle are input through the keyboard. 
 Write a program to calculate the area & perimeter of the rectangle, and the area & circumference of the circle.*/
 
 
